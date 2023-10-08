455607e83a6d974aa1dc49c6021a77014d59a78b
Gianluca Rossi <gr.gianlucarossi@gmail.com>
2023-10-08 09:24:05 -0400

    use local model

--- memory/start.py
+++ memory/start.py
@@ -1,13 +1,12 @@
 from pathlib import Path
 
 import dotenv
-from langchain.chains import LLMChain
+from langchain import hub
 from langchain.document_loaders import PyPDFLoader
 from langchain.embeddings.sentence_transformer import SentenceTransformerEmbeddings
-from langchain.llms import HuggingFaceHub
-from langchain.llms.base import LLM
-from langchain.prompts import PromptTemplate
-from langchain.text_splitter import CharacterTextSplitter
+from langchain.llms import HuggingFacePipeline
+from langchain.schema.runnable import RunnablePassthrough
+from langchain.text_splitter import RecursiveCharacterTextSplitter
 from langchain.vectorstores import Chroma, VectorStore
 
 
@@ -15,23 +14,6 @@ dotenv.load_dotenv()
 embedding_function = SentenceTransformerEmbeddings(model_name="BAAI/bge-large-en")
 VECTOR_DB_PATH = "./chroma_db"
 MODEL_PATH = "/Users/rlm/Desktop/Code/llama.cpp/models/llama-2-13b-chat.ggufv3.q4_0.bin"
-
-
-def index_data(embedding_function=embedding_function) -> VectorStore:
-    # load the document and split it into chunks
-    loader = PyPDFLoader("~/Downloads/andrew-ng-machine-learning-yearning-1.pdf")
-    documents = loader.load()
-
-    # split it into chunks
-    text_splitter = CharacterTextSplitter(chunk_size=1_000, chunk_overlap=0)
-    docs = text_splitter.split_documents(documents)
-
-    # create the open-source embedding function
-
-    # load it into Chroma
-    return Chroma.from_documents(
-        docs, embedding_function, persist_directory=VECTOR_DB_PATH
-    )
 
 
 def get_search_index() -> VectorStore:
@@ -45,34 +27,37 @@ def get_search_index() -> VectorStore:
         return index_data()
 
 
+def index_data(embedding_function=embedding_function) -> VectorStore:
+    # load the document and split it into chunks
+    loader = PyPDFLoader("~/Downloads/andrew-ng-machine-learning-yearning-1.pdf")
+    documents = loader.load()
+
+    # split it into chunks
+    text_splitter = RecursiveCharacterTextSplitter(chunk_size=100, chunk_overlap=20)
+    chunks = text_splitter.split_documents(documents)
+
+    # load it into Chroma
+    return Chroma.from_documents(
+        chunks, embedding_function, persist_directory=VECTOR_DB_PATH
+    )
+
+
 def main():
     search_index = get_search_index()
-    from langchain.chains import LLMChain
-    from langchain.llms import HuggingFaceHub
+    retriever = search_index.as_retriever()
 
-    from langchain.prompts import PromptTemplate
-
-    question = "Who won the FIFA World Cup in the year 1994? "
-    template = """Question: {question}
-
-    Answer: Let's think step by step."""
-    prompt = PromptTemplate(template=template, input_variables=["question"])
-
-    repo_id = "google/flan-t5-xxl"  # See https://huggingface.co/models?pipeline_tag=text-generation&sort=downloads for some other options
-    llm = HuggingFaceHub(
-        repo_id=repo_id, model_kwargs={"temperature": 0.5, "max_length": 264}
+    rag_prompt = hub.pull("rlm/rag-prompt")
+    llm = HuggingFacePipeline.from_model_id(
+        model_id="bigscience/bloom-1b7",
+        task="text-generation",
+        model_kwargs={"temperature": 0, "max_length": 64},
     )
-    llm_chain = LLMChain(prompt=prompt, llm=llm)
+    rag_chain = (
+        {"context": retriever, "question": RunnablePassthrough()} | rag_prompt | llm
+    )
 
-    print(llm_chain.run(question))
-
-    # # query it
-    # while True:
-    #     query = input("Input: ")
-    #     docs = search_index.similarity_search(query)
-    #     # print results
-    #     for doc in docs:
-    #         print(["".join(doc.page_content).replace("\n", "")])
+    while True:
+        rag_chain.invoke("What is Task Decomposition?")
 
 
 if __name__ == "__main__":
